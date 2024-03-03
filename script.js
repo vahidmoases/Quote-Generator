02f59f46cd8595460a6293c279d62442e60cc48f
vahid moases ghafary <135539904+vahidmoases@users.noreply.github.com>
2024-03-03 17:15:42 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_Url = "https://api.quotable.io/random"; 
+
+async function getquote(url){
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;
+}
+getquote(api_Url);
